7fe06c05256c203af9b70cd67d812c56764b027e
Sakib Islam <bachelorsakib@gmail.com>
2023-08-15 02:37:26 +0600

    added

--- withdraw.js
+++ withdraw.js
@@ -7,15 +7,21 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
    const withdrawTotalElement = document.getElementById('withdraw-f');
    const previousWithdrawString = withdrawTotalElement.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawString);
-   
-   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-     withdrawTotalElement.innerText = currentWithdrawTotal;
 
-    withdrawField.value = '';
+    
 
    const balanceTotalElement = document.getElementById('balance-total');
    const previouBalanceTotalString = balanceTotalElement.innerText;
    const previouBalanceTotal = parseFloat(previouBalanceTotalString);
+
+   withdrawField.value = '';
+   
+   if(newWithdrawAmount > previouBalanceTotal){
+      return alert('Not available this amount of money on you account')
+    }
+       
+   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+   withdrawTotalElement.innerText = currentWithdrawTotal;
 
    const newBalanceTotal = previouBalanceTotal - newWithdrawAmount;
    balanceTotalElement.innerText = newBalanceTotal;
